4732151e6bd058359895b57ca7a920b6bf19f190
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-22 14:24:01 +0200

    Add files via upload

---
+++ Queue program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Queue <int> queue = new Queue<int>(); // för att skapa en kö av något
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+            
+        queue.Enqueue(5);
+            
+        queue.Enqueue(6);
+
+        Console.WriteLine($"Det första elementet i kön är {queue.Peek()}"); // för att visa en del av kön
+
+        queue.Dequeue();
+        queue.Dequeue();
+
+        Console.WriteLine($"Det första elementet i kön är {queue.Peek()}");
+    }
+}
---
+++ Queue program/Queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_program
+{
+
+    public class Queue <T>
+    {
+        private List<T> elements = new List<T>();
+        public void Enqueue(T item)
+        {
+            elements.Add(item);
+
+            Console.WriteLine($"{item} har lagts till i kön");
+
+        }
+
+        public T Dequeue()
+        {
+            if(IsEmpty())
+            {
+                throw new InvalidOperationException("Kön är tom");
+
+            }
+
+            T value = elements[0];
+            elements.RemoveAt(0);
+            Console.WriteLine($"{value} har tagits bort från kön");
+            return value;
+
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Kön är tom");
+            }
+
+            return elements[0];
+            
+        }
+
+        public bool IsEmpty()
+        {
+
+            return elements.Count == 0;
+        }
+    }
+}
